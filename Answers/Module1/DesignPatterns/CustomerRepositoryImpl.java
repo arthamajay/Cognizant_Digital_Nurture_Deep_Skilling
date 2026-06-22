89fd7941fda78bba44d30ed299ea589d2877c9d7
Ajay <arthamajay7@gmail.com>
2026-06-22 23:50:50 +0530

    Module -1 Completed

--- Answers/Module1/DesignPatterns/CustomerRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-public class CustomerRepositoryImpl implements CustomerRepository {
-    public String findCustomerById(int id) {
-        if(id==1) return "Alice";
-        if(id==2) return "Bob";
-        return "Customer Not Found";
-    }
-}
